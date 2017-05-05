868f3949f88708dca6bd64f714e303c528392779
Timothy Soon Yu Feng <docterfurry@hotmail.com>
2017-05-05 13:22:36 +0800

    Created Main Menu
    Added Main Menu Scene

---
+++ Assets/Menu Script/SceneSkipper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSkipper : MonoBehaviour {
+
+    public void gotoTrainerMode() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void gotoTraineeMode()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
